c41fcf5c915e0ea34a70fe6c659ed27d027cd815
Tobias Lohse <tolo@TobisMBA.local>
2013-05-18 16:59:26 +0200

    No message

---
+++ index.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html>
+	<!-- Head -->
+	<head> 
+		<meta charset="utf-8"> 
+		<title>
+			Tobis Blog
+		</title>
+		<link rel="stylesheet" href="main.css">
+		<link href="http://fonts.googleapis.com/css?family=Ubuntu:300,500,300italic" rel="stylesheet" type="text/css">
+	</head>
+	<!-- Body -->
+	<body>
+		<div id="center">
+			<?php 
+				include "navs.html";
+			?>
+			<!-- Title -->
+			<div class="title">
+				<img src="ressources/title/blog_title.png" alt="BLOG">
+				<p>
+					<span><a href="under_construction.html">- SEARCH -</a></span><span><a href="under_construction.html">- TAGS -</a></span> <span><a href="under_construction.html">- ARCHIVE -</a></span>
+				</p>
+			</div>
+
+			<!-- Page 1 -->
+			<div class="page">
+				<p class="date"> 
+					<span>29th April 2012</span>
+				</p>
+				<h1> 
+					<a href="under_construction.html">Titulum Primum </a>
+				</h1>
+				<p>
+					Lorem ipsum <a href="http://www.w3schools.com/tags/tag_a.asp">dolor</a> sit amet, consectetur adipiscing elit, set eiusmod tempor incidunt et labore et dolore magna aliquam. Ut enim ad minim veniam, quis nostrud exerc. Irure dolor in reprehend incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+				</p>
+				<div class="frame"> 
+					<span class="tl"></span><span class="tr"></span><span class="bl"></span><span class="br"></span>
+					<img src="img/pic4.jpg" alt="Foto of the Bahai">
+				</div>
+				<p>
+					Duis aute irure dolor in reprehenderit in voluptate velit esse molestaie cillum. Tia non ob ea soluad incom dereud facilis est er expedit distinct. Nam liber te conscient to factor tum poen legum odioque civiuda et tam. Neque pecun modut est neque nonor et imper ned libidig met, consectetur adipiscing elit, sed ut labore et dolore magna aliquam is nostrud exercitation ullam mmodo consequet misericordiam meam deum ibi:
+				</p>
+				<ul>
+					<li> At vver eos et accusam dignissum qui blandit est praesent. Trenz pruca beynocguon doas nog apoply. </li>
+					<li> Su trenz ucu hugh rasoluguon monugor or trenz ucugwo jag scannar.</li>
+					<li> Wa hava laasad trenzsa gwo producgs su IdfoBraid. </li>
+				</ul>
+				<p>
+					Yop quiel geg ba solaly rasponsubla rof trenzur sala ent dusgrubuguon. Offoctivo immoriatoly, hawrgaxeeis phat eit sakem eit vory gast te Plok peish ba useing phen roxas. Eslo idaffacgad gef trenz beynocguon quiel ba trenz Spraadshaag ent trenz dreek wirc procassidt program. Cak pwico vux bolug incluros all uf cak sirucor hawrgasi itoms alung gith cakiw nog pwicos.
+				</p>
+				<p>
+					Plloaso mako nuto uf cakso dodtos anr koop a cupy uf cak vux noaw yerw phuno. Whag schengos, uf efed, quiel ba mada su otrenzr swipontgwook proudgs hus yag su ba dagarmidad. Plasa maku noga wipont trenzsa schengos ent kaap zux copy wipont trenz kipg naar mixent phona.
+				</p>
+				<?php 
+					include "share.html"; 
+				?>
+				<div class="tags"><div>
+					<a href="">Evanston</a><a href="">Photography</a>
+				</div></div>
+			</div>
+
+			<!-- Page 2 -->
+			<div class="page">
+				<p class="date"> 
+					<span>15th September 2012</span>
+				</p>
+				<h1> 
+					<a href="under_construction.html">Segundum Titulum Blogarium Novum</a>
+				</h1>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas, commodi, consequatur deserunt atque iste ab labore aliquam eligendi debitis doloremque impedit vitae praesentium id harum autem tenetur consectetur exercitationem quasi. 
+				</p>
+				<p>
+					<span>Optio delectus modi corrupti dignissimos eveniet dolor pariatur. Aperiam, dolorum, non error expedita doloremque voluptatum necessitatibus labore pariatur odio eaque animi quaerat voluptatem iste unde excepturi itaque assumenda eum cumque!</span>
+				</p>
+				<h2>
+					Lorem Ipsum Titularium
+				</h2>
+				<p>
+					Accusantium, quae, excepturi velit ducimus reprehenderit nisi minima unde nesciunt esse iure numquam hic! Accusamus, totam, sunt iure hic neque impedit quaerat doloribus earum obcaecati velit unde dolores voluptatem deleniti! Sint, ipsum, facilis, vel porro totam odit eveniet blanditiis explicabo minima dignissimos ducimus praesentium nemo maxime! Deserunt, dignissimos cumque sed quos ipsum eum suscipit. Perspiciatis dignissimos nisi dicta sunt ipsam. Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+				</p>
+				<p>
+					Yop quiel geg ba solaly rasponsubla rof trenzur sala ent dusgrubuguon. Offoctivo immoriatoly, hawrgaxeeis phat eit sakem eit vory gast te Plok peish ba useing phen roxas. Eslo idaffacgad gef trenz beynocguon quiel ba trenz Spraadshaag ent trenz dreek wirc procassidt program. Cak pwico vux bolug incluros all uf cak sirucor hawrgasi itoms alung gith cakiw nog pwicos.
+				</p>
+				<div class="frame narrow"> 
+					<span class="tl"></span><span class="tr"></span><span class="bl"></span><span class="br"></span>
+					<img src="img/pic5.jpg" alt="Foto of Red Flowers">
+				</div>
+				<p>
+					Plloaso mako nuto uf cakso dodtos anr koop a cupy uf cak vux noaw yerw phuno. Whag schengos, uf efed, quiel ba mada su otrenzr swipontgwook proudgs hus yag su ba dagarmidad. Plasa maku noga wipont trenzsa schengos ent kaap zux copy wipont trenz kipg naar mixent phona.
+				</p>
+				<?php 
+					include "share.html"; 
+				?>
+				<div class="tags"><div>
+					<a href="">Theology</a><a href="">Kierkegaard</a><a href="">Philosophy</a><a href="">Opinion</a><a href="">Faith</a><a href="">Way too many tags, which should be avoided!</a><a href="">But they will work fine with this scroll feature.</a>
+				</div></div>
+			</div>
+
+			<?php 
+				include("footer.html");
+			?>
+		</div>
+	</body>
+</html>
---
+++ test.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<p>
+		this is some text
+	</p>
+	<?php 
+		echo "test <br><br>" 
+	?>
+	 <?php 
+	 	include("include.html") 
+	 ?>
+</body>
+</html>

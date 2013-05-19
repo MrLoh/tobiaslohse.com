7659bde4fca3274b4c8c6b9aca47f33c9cd4945c
Tobias Lohse <tolo@TobisMBA.local>
2013-05-19 17:21:59 +0200

    Implementation of Codeboxes with Syntax highlighting via Google code prettify

--- index.php
+++ index.php
@@ -9,6 +9,8 @@
 		<link rel="stylesheet" href="main.css">
 		<link href="http://fonts.googleapis.com/css?family=Ubuntu:300,500,300italic" rel="stylesheet" type="text/css">
 	</head>
+<!-- 	<script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script> -->
+	<script src="https://google-code-prettify.googlecode.com/svn/loader/run_prettify.js"></script>
 	<!-- Body -->
 	<body>
 		<div id="center">
@@ -34,9 +36,9 @@
 				<p>
 					Lorem ipsum <a href="http://www.w3schools.com/tags/tag_a.asp">dolor</a> sit amet, consectetur adipiscing elit, set eiusmod tempor incidunt et labore et dolore magna aliquam. Ut enim ad minim veniam, quis nostrud exerc. Irure dolor in reprehend incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
 				</p>
-				<div class="frame"> 
+				<div class="frame narrow"> 
 					<span class="tl"></span><span class="tr"></span><span class="bl"></span><span class="br"></span>
-					<img src="img/pic4.jpg" alt="Foto of the Bahai">
+					<img src="img/pic2.jpg" alt="Foto of the Bahai">
 				</div>
 				<p>
 					Duis aute irure dolor in reprehenderit in voluptate velit esse molestaie cillum. Tia non ob ea soluad incom dereud facilis est er expedit distinct. Nam liber te conscient to factor tum poen legum odioque civiuda et tam. Neque pecun modut est neque nonor et imper ned libidig met, consectetur adipiscing elit, sed ut labore et dolore magna aliquam is nostrud exercitation ullam mmodo consequet misericordiam meam deum ibi:
@@ -83,10 +85,27 @@
 				<p>
 					Yop quiel geg ba solaly rasponsubla rof trenzur sala ent dusgrubuguon. Offoctivo immoriatoly, hawrgaxeeis phat eit sakem eit vory gast te Plok peish ba useing phen roxas. Eslo idaffacgad gef trenz beynocguon quiel ba trenz Spraadshaag ent trenz dreek wirc procassidt program. Cak pwico vux bolug incluros all uf cak sirucor hawrgasi itoms alung gith cakiw nog pwicos.
 				</p>
-				<div class="frame narrow"> 
+				<div class="frame caption"> 
 					<span class="tl"></span><span class="tr"></span><span class="bl"></span><span class="br"></span>
-					<img src="img/pic5.jpg" alt="Foto of Red Flowers">
+					<img src="img/pic6.jpg" alt="Foto of Red Flowers">
 				</div>
+				<p class="caption">
+					flowers at the lakeside
+				</p>
+				<div class="frame caption"> 
+					<span class="tl"></span><span class="tr"></span><span class="bl"></span><span class="br"></span>
+					<img src="img/pic7.jpg" alt="Foto of Red Flowers">
+				</div>
+				<p class="caption">
+					with friends in Door County
+				</p>
+				<div class="frame caption"> 
+					<span class="tl"></span><span class="tr"></span><span class="bl"></span><span class="br"></span>
+					<img src="img/pic8.jpg" alt="Foto of Red Flowers">
+				</div>
+				<p class="caption">
+					snake eating a frog 
+				</p>
 				<p>
 					Plloaso mako nuto uf cakso dodtos anr koop a cupy uf cak vux noaw yerw phuno. Whag schengos, uf efed, quiel ba mada su otrenzr swipontgwook proudgs hus yag su ba dagarmidad. Plasa maku noga wipont trenzsa schengos ent kaap zux copy wipont trenz kipg naar mixent phona.
 				</p>
@@ -97,6 +116,52 @@
 					<a href="">Theology</a><a href="">Kierkegaard</a><a href="">Philosophy</a><a href="">Opinion</a><a href="">Faith</a><a href="">Way too many tags, which should be avoided!</a><a href="">But they will work fine with this scroll feature.</a>
 				</div></div>
 			</div>
+
+			<!-- Page 3 -->
+			<div class="page">
+				<p class="date"> 
+					<span>15th September 2012</span>
+				</p>
+				<h1> 
+					<a href="under_construction.html">Some Code Example</a>
+				</h1>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas, commodi, consequatur deserunt atque iste ab labore aliquam eligendi debitis doloremque impedit vitae praesentium id harum autem tenetur consectetur exercitationem quasi. 
+				</p>
+				<pre class="prettyprint linemus"><code><div>
+&lt;script type="text/javascript">
+// Say hello world until the user starts questioning the meaningfulness of their existence. 
+function helloWorld(world) {
+  for (var i = 42; --i >= 0;) {
+    alert('Hello ' + String(world));
+  }
+}
+&lt;/script>
+
+&lt;style>
+p { 
+  color: pink 
+}
+
+pre {
+  @extend code;
+  margin: 2em -0.5em 2em -0.5em;
+  box-shadow: inset 0 0 2em 0.2em $transparent;
+  background: white url("ressources/back.png");
+}
+&lt;/style>
+				</div></code></pre>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. At, error qui assumenda tempora quaerat molestiae voluptatum iste suscipit veritatis aut atque animi doloremque velit saepe ab explicabo ullam impedit aliquam.
+				</p>
+				<?php 
+					include "share.html"; 
+				?>
+				<div class="tags"><div>
+					<a href="">Coding</a><a href="">CSS</a><a href="">Hello World</a>
+				</div></div>
+			</div>
+
 
 			<?php 
 				include("footer.html");
